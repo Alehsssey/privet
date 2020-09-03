c73cbdb2fbae768450b551c09678c49106d65f97
Alehsssey <lehadolya612@gmail.com>
2020-09-03 09:33:41 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace blablabla
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Дмитро Сясько!");
         }
     }
 }
